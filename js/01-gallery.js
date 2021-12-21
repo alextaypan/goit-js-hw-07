bd87b431f0148e6d896d603c16fd66bc1629625b
Oleksandr Yakob <yakoboleksandr@gmail.com>
2021-12-21 18:13:12 +0200

    complete task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+    </li> `;
+  })
+  .join("");
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+let instance = null;
+galleryRef.addEventListener("click", onOpenModal);
+function onOpenModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+  const dataSrc = event.target.dataset.source;
+  console.log(dataSrc);
+  instance = basicLightbox.create(
+    `<img src="${dataSrc}" width="800" height="600">`
+  );
+  instance.show();
+  document.addEventListener("keydown", onCloseModal);
+}
+
+function onCloseModal(event) {
+  if (event.code === "Escape") instance.close();
+  document.removeEventListener("keydown", onCloseModal);
+}
